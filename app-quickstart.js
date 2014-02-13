f13442b7fb1cec5091f224a6161b71cb5cdb48d1
Kim Horakh <kimh@zoovy.com>
2014-02-13 14:53:58 -0800

    Wiring mods now loads diagram category layout

--- app-quickstart.js
+++ app-quickstart.js
@@ -2786,7 +2786,7 @@ buyer to 'take with them' as they move between  pages.
 					else if(catSafeID == zGlobals.appSettings.rootcat || infoObj.pageType == 'homepage')	{
 						infoObj.templateID = 'homepageTemplate'
 						}
-					else if(catSafeID == '.wiring_resources_guitar_wiring_diagrams.1_pickup_guitar_wiring_diagrams'){
+					else if(catSafeID == '.wiring_resources_guitar_wiring_diagrams.1_pickup_guitar_wiring_diagrams' || catSafeID == ".wiring_resources_guitar_wiring_diagrams.guitar_wiring_mods"){
 						infoObj.templateID = 'categoryTemplateWiringDiagram';
 						}
 					else	{
